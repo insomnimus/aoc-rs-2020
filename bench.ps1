ac8d8b70a7462b7165a79aa3616b546d02073a11
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-08-03 20:04:00 +0300

    feat: added bench.ps1

---
+++ bench.ps1
@@ -0,0 +1,23 @@
+function new-tempdir{
+    $parent = [System.IO.Path]::GetTempPath()
+    [string] $name = [System.Guid]::NewGuid()
+    New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+echo "building everything in release mode"
+$dir=new-tempdir
+cargo build --release --bins --target-dir $dir
+
+if($lastExitCode) {
+	return "could not compile the project"
+}
+
+$bins= get-childitem "$dir\release\day*.exe"
+
+$t= measure-command {
+	$bins | % { & $_ }
+}
+
+echo "note: benchmarks on wsl perform many times better, this may have to do with the way powershell's measure-command measures time" 1>&2
+
+$t
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Advent of Code 2020
+
+These are my solutions to Advent of Code year 2020 in rust.
+
+There's a shell script (`bench.sh` and `bench.ps1`) that builds everything in release profile and runs them sequentially, 
+measuring the time it takes from day 1 to the last one if you want to check the performance.
